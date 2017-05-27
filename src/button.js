a21312398b2a8701b85adf645581d5cf405d5e0f
Brandon Dail <brandon.dail@formidable.com>
2017-05-27 10:50:04 -0500

    Reformat button.js with new prettier settings

--- src/button.js
+++ src/button.js
@@ -1,13 +1,13 @@
 // @flow
 import { h } from "preact";
+import type { Sizes } from "./shared.js";
 
-type ButtonSize = "xsmall" | "small" | "medium" | "large" | "xlarge" | "super";
 type ButtonType = "brand" | "info" | "warning" | "success" | "error";
 
 type ButtonProps = {
   label: string,
   type: ?ButtonType,
-  size: ?ButtonSize,
+  size: ?Sizes,
   ghost: ?boolean,
   block: ?boolean,
   className: ?string,
@@ -24,13 +24,7 @@ const getButtonClassName = (
   rounded,
   className
 ): string =>
-  `c-button${type
-    ? type && ghost ? ` c-button--ghost-${type}` : ` c-button--${type}`
-    : ""}${ghost && !type ? " c-button--ghost" : ""}${block
-    ? " c-button--block"
-    : ""}${size ? ` u-${size}` : ""}${className ? className : ""}${rounded
-    ? " c-button--rounded"
-    : ""}`;
+  `c-button${type ? (type && ghost ? ` c-button--ghost-${type}` : ` c-button--${type}`) : ""}${ghost && !type ? " c-button--ghost" : ""}${block ? " c-button--block" : ""}${size ? ` u-${size}` : ""}${className ? className : ""}${rounded ? " c-button--rounded" : ""}`;
 
 const Button = (props: ButtonProps) => {
   const { label, children, ...other } = props;
