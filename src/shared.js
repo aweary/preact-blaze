321beef0f436162553012930539686c13b4c2932
Brandon Dail <brandon.dail@formidable.com>
2017-05-27 10:50:32 -0500

    Add shared types module

---
+++ src/shared.js
@@ -0,0 +1,11 @@
+// @flow
+
+export type Sizes =
+  | "xsmall"
+  | "small"
+  | "medium"
+  | "large"
+  | "xlarge"
+  | "super";
+
+export type StyleType = "brand" | "info" | "error" | "warning" | "success";
