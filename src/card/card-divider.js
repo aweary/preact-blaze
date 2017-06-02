5b6ef043d1c4d761fee012998308d640c2c4817d
Brandon Dail <brandon.dail@formidable.com>
2017-06-02 16:24:39 -0500

    Add CardDivider component

---
+++ src/card/card-divider.js
@@ -0,0 +1,7 @@
+// @flow
+import { h } from "preact";
+import "blaze/dist/components.cards.min.css";
+
+const CardItemDivider = () => <div className="c-card__divider" />;
+
+export default CardItemDivider;
